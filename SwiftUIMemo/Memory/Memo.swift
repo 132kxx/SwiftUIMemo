0a42825f0d1cb7d8880037284723b5a71019c8c9
kxx <132kxx@gmail.com>
2022-07-14 21:43:10 +0900

    step2

---
+++ SwiftUIMemo/Memory/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by kxx: on 2022/07/14.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let inserDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.inserDate = insertDate
+    }
+}
